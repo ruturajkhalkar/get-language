2e6d8522c16250958290f657f2d27507459b7170
ruturajkhalkar <ruturajkhalkar@gmail.com>
2024-04-03 00:41:12 +0530

    initail commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const languageMaster = [
+    {
+        "languageid": 1,
+        "language": "English",
+        "languagecode": "en"
+    },
+    {
+        "languageid": 2,
+        "language": "Hindi",
+        "languagecode": "hi"
+    },
+    {
+        "languageid": 3,
+        "language": "Marathi",
+        "languagecode": "ar"
+    },
+    {
+        "languageid": 4,
+        "language": "Gujarati",
+        "languagecode": "gu"
+    }
+];
+
+
+
+export default languageMaster;
+
